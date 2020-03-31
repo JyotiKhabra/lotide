e4346880d034f4c9eeb4201c3811fb15e39fe68c
vagrant <j_khabra11@hotmail.com>
2020-03-31 04:25:51 +0000

    template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function (actual, expected) {
 
   if (actual === expected) {
-    console.log("U+1F64C " + " Asssertion Passed: " + actual + " === " + expected);
+    console.log(`U+1F64C Asssertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("U+1F612 " + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`U+1F612 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
