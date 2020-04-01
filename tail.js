feb01788b549105732b1b186da2546611ca328a3
vagrant <j_khabra11@hotmail.com>
2020-04-01 02:12:24 +0000

    working emojis addition

--- tail.js
+++ tail.js
@@ -1,10 +1,11 @@
 const assertEqual = function (actual, expected) {
 
   if (actual === expected) {
-    console.log(`U+1F64C Asssertion Passed: ${actual} === ${expected}`);
+    console.log(`😉 Asssertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`U+1F612 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
   }
+
 };
 const tail = function (array) {
   return array.slice(1);
