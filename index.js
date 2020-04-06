d36a04ee59c14ef8f6f7d59091bec003d8c991ad
vagrant <j_khabra11@hotmail.com>
2020-04-06 21:58:24 +0000

    ES6 version

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  assertArraysEqual:  assertArraysEqual,
-  assertEqual:        assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters:       countLetters,
-  eqArrays:           eqArrays,
-  eqObjects:          eqObjects,
-  findKeyByValue:     findKeyByValue,
-  findKeys:           findKeys,
-  head:               head,
-  letterPositions:    letterPositions,
-  map:                map,
-  middle:             middle,
-  tail:               tail,
-  takeUntil:          takeUntil,
-  without:            without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  findKeys,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 }
